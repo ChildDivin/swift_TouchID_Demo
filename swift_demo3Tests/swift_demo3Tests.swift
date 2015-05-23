b4c6350b839201eca886f879a7c56a7ba21776f1
Tops <tops@Tops31.local>
2015-05-23 14:17:44 +0530

    Initial Commit

---
+++ swift_demo3Tests/swift_demo3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_demo3Tests.swift
+//  swift_demo3Tests
+//
+//  Created by Tops on 5/23/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swift_demo3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
